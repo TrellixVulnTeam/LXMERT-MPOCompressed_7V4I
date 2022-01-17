e4b7a28bca50624f966dfe4f60ba43a4ed4efcfb
jasonliu <jason8121@foxmail.com>
2022-01-17 15:56:49 +0800

    fix Model2MPO

--- src/compress_tools/Model2MPO.py
+++ src/compress_tools/Model2MPO.py
@@ -4,7 +4,7 @@ from compress_tools.Linear2MPO import Linear2MPO
 from torch import nn
 
 
-def Model(module, prefix=''):
+def Model2Mpo(module, prefix=''):
     for name, child in module._modules.items():
         if child is not None:
             newmodule = getattr(module, name)
@@ -12,5 +12,5 @@ def Model(module, prefix=''):
                 mpo_module = Linear2MPO(newmodule, tensor_learn=True)
                 mpo_module.from_pretrained(newmodule)
                 setattr(module, name, mpo_module)
-            Model2Mpo(child, prefix + name + 
+            Model2Mpo(child, prefix + name + '.')
     
